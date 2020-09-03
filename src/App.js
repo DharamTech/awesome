925405298cb3eb549e7008d91dd2b2a671113eda
DharamTech <dharminderkumar0475@gmail.com>
2020-09-03 09:44:58 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home.js'
 import About from './pages/About.js'
-import 'react-responsive-carousel/lib/styles/carousel.min.css'
-import {carousel} from 'react-responsive-carousel'
+
 
 class App extends Component {
   render() {
